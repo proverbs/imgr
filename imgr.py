a1dd495acfb2318752292a09318cc221cef46fce
Hao Xu <proverbsonly@gmail.com>
2020-07-25 22:35:58 -0700

    support: imgr mkdir <relative-dir>

--- imgr.py
+++ imgr.py
@@ -17,18 +17,37 @@ def _init():
             click.echo(f'Using local base: {_LOCAL_BASE}')
 
 
+def _check_configuration():
+    global _LOCAL_BASE
+    if _LOCAL_BASE is None:
+        raise ValueError('Please run `imgr config` first!')
+
+
 @click.command()
 @click.option('--local-base',
               type=click.Path(exists=True),
               required=True,
               help='Local dir for image hosting. Must be a git base dir.')
 def configure(local_base):
+    """Configure local base dir for image hosting."""
     base_dir = os.path.abspath(local_base)
     if not os.path.isdir(base_dir):
-        raise ValueError('Error: Please specify a dir!')
+        raise ValueError('Please specify a dir!')
     with open(_CONFIGURE_PATH, 'w') as f:
         json.dump({'local-base': base_dir}, f)
         click.echo(f'Local base is set successfully to {base_dir}')
+
+
+@click.command()
+@click.argument('dir-name', type=str, required=True)
+def mkdir(dir_name):
+    """Create dir in the local base."""
+    _check_configuration()
+    new_dir = os.path.join(_LOCAL_BASE, dir_name)
+    if os.path.exists(new_dir):
+        raise ValueError(f'{new_dir} already exists!')
+    os.makedirs(new_dir)
+    click.echo(f'New dir created: {new_dir}')
 
 
 @click.group()
@@ -37,6 +56,7 @@ def cli():
 
 
 cli.add_command(configure)
+cli.add_command(mkdir)
 
 if __name__ == '__main__':
     _init()
